1f085355d6fc8c9cfbd6f20f8405c22b7074d904
mnovak-20 <matthew.novak@staffs.ac.uk>
2025-06-23 11:29:13 +0100

    image location

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ export default function AwesomeList() {
   return (
     <div className="bg-[#F8F8F8] min-h-screen p-8 font-sans">
       <header className="text-center mb-10">
-        <img src="/images/UofS_Master_Logo_RGB_Reverse.png" alt="Staffordshire University Logo" className="w-40 mx-auto mb-4" />
+        <img src="LoA_Staffs/images/UofS_Master_Logo_RGB_Reverse.png" alt="Staffordshire University Logo" className="w-40 mx-auto mb-4" />
         <h1 className="text-4xl font-bold text-[#C8102E]">The List of Awesome</h1>
         <h2 className="text-lg text-gray-700 mt-2">Game Credit list of Staffordshire Alumni</h2>
       </header>
